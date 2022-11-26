85138dbe04b81e812437b79228caf542907e172e
matteouni <118944004+matteouni@users.noreply.github.com>
2022-11-26 15:03:47 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,57 @@
+require('packer').startup(function(use)
+    use "wbthomason/packer.nvim"
+    use "github/copilot.vim"
+end)
+-- config centered on github copilot
+local options = {
+    clipboard = vim.opt.clipboard + "unnamedplus" + "unnamed",
+    expandtab = true,
+    hlsearch = false,
+    iskeyword = vim.opt.iskeyword + "-" + "_",
+    number = true,
+    pumheight = 7,
+    relativenumber = true,
+    scrolloff = 999,
+    shiftwidth = 4, -- Number of spaces to use for each step of (auto)indent
+    showmode = false,
+    smartindent = true, -- Do smart autoindenting when starting a new line
+    softtabstop = 4, -- Number of spaces that a <Tab> counts for while performing editing operations
+    tabstop = 4, -- Number of spaces that a <Tab> in the file counts for
+    updatetime = 200,
+}
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+-- partiallly accept GitHub copilot suggestion, word by word
+local function partiallyAccept()
+    local _ = vim.fn['copilot#Accept']("")
+    local suggestion = vim.fn['copilot#TextQueuedForInsertion']()
+    return vim.fn.split(suggestion,  [[[ .]\zs]])[1] -- the word can be separated by space or by the dot char
+end
+
+local function keymap(modes, key, value, opts)
+    for i=1, string.len(modes) do
+        local mode = string.sub(modes, i, i)
+        vim.keymap.set(mode, key, value, opts)
+    end
+end
+local function smartIndent()
+    local currLine, currCol = unpack(vim.api.nvim_win_get_cursor(0))
+    --! => nore
+    vim.api.nvim_command("normal! gg=G")
+    vim.api.nvim_command("normal! " .. currLine .. "G")
+end
+-- TODO indent mode
+local keymaps = { -- :h modes
+    {"nv", "<leader>p", '"+p', {noremap=true}},
+    {"nv", "<leader>y", '"+y', {noremap=true}},
+    {"nv", "c", '"_c', {noremap=true}},
+    {"nv", "d", '"_d', {noremap=true}},
+    {"nv", "x", '"_x', {noremap=true}},
+    {"i", "<c-l>", partiallyAccept, {expr=true}},
+    {"n", "<c-f>", smartIndent, {}},
+}
+for _, v in pairs(keymaps) do
+    keymap(unpack(v))
+end
